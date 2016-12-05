4231c11fb20fdcc8bf9831f40c014a371cf78cd1
10eputzen <stef.development@gmail.com>
2016-12-06 00:45:48 +0100

    Initial Map and Some Knockout

--- app/js/app.js
+++ app/js/app.js
@@ -1,47 +1,102 @@
-/* off-canvas sidebar toggle */
-$('[data-toggle=offcanvas]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-    $('.collapse').toggleClass('in').toggleClass('hidden-xs').toggleClass('visible-xs');
+// add to footer
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
 });
 
+var self = this;
 
 var map;
-// Function to initialize the map within the map div
+
+ko.applyBindings({
+    listOfObject: [{
+        position: {
+            lat: 50.08877,
+            lng: 8.4579088,
+        },
+        map: map,
+        title: 'Feuerwehr'
+
+    }, {
+        position: {
+            lat: 50.09071,
+            lng: 8.4603596
+        },
+        map: map,
+        title: 'Polizei'
+    }]
+});
+
+ko.applyBindings(new AppViewModel());
+
+ko.applyBindings({
+    people: [
+        { firstName: 'Bert', lastName: 'Bertington' },
+        { firstName: 'Charles', lastName: 'Charlesforth' },
+        { firstName: 'Denise', lastName: 'Dentiste' }
+    ]
+});
+
 function initMap() {
+
+    var home = {
+        lat: 50.0900539,
+        lng: 8.4624332
+    };
+
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: 40.74135,
-            lng: -73.99802
+            lat: home.lat,
+            lng: home.lng
         },
-        zoom: 14
+        zoom: 16
     });
-    // Create a single latLng literal object.
-    var singleLatLng = {
-        lat: 40.74135,
-        lng: -73.99802
-    };
-    // TODO: Create a single marker appearing on initialize -
-    // Create it with the position of the singleLatLng,
-    // on the map, and give it your own title!
-    var marker = new google.maps.Marker({
-        position: singleLatLng,
+
+
+    var markers = [];
+    var contents = [];
+    var infowindows = [];
+
+    for (i = 0; i < self.listOfObject.length; i++) {
+        markers[i] = new google.maps.Marker({
+            position: self.listOfObject[i].position,
+            map: map,
+            title: 'marker'
+        });
+
+        markers[i].index = i;
+        contents[i] = '<div class="popup_container">' + self.listOfObject[i].title + '</div>';
+
+
+        infowindows[i] = new google.maps.InfoWindow({
+            content: contents[i],
+            maxWidth: 300
+        });
+
+        google.maps.event.addListener(markers[i], 'click', function() {
+            infowindows[this.index].open(map, markers[this.index]);
+            map.panTo(markers[this.index].getPosition());
+        });
+    }
+
+
+    var marker_home = new google.maps.Marker({
+        position: home,
         map: map,
         icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-        title: 'Hello World!'
+        title: 'Work'
     });
 
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
+
+
+    marker_home.addListener('click', function() {
+        infowindow.open(map, marker_home);
+        map.panTo(marker_home.getPosition());
+
     });
 
-    var contentString = '<label> Lat: ' + singleLatLng.lat + ' Long: ' + singleLatLng.lng;
     var infowindow = new google.maps.InfoWindow({
-        content: contentString
+        content: '<div class="popup_container">' + marker_home.title + '</div>'
     });
 
-    // TODO: create a single infowindow, with your own content.
-    // It must appear on the marker
-
-    // TODO: create an EVENT LISTENER so that the infowindow opens when
-    // the marker is clicked!
 }
